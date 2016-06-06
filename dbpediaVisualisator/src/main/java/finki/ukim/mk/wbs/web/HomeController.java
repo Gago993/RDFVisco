7b0ea0c26e29b63265e5ec8f82eec06b95b12b59
Gago <g.ognjanovski@hotmail.com>
2016-06-06 17:16:36 +0200

    Initial Project Structure

---
+++ dbpediaVisualisator/src/main/java/finki/ukim/mk/wbs/web/HomeController.java
@@ -0,0 +1,20 @@
+package finki.ukim.mk.wbs.web;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/first", method = RequestMethod.GET)
+	public ModelAndView getFirst() {
+		ModelAndView m = new ModelAndView("first");
+		return m;
+	}
+}
